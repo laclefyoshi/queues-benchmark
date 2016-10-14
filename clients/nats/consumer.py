565450a65efe27b616ef10ca3610c045b7c72c12
ysaeki <ysaeki@r.recruit.co.jp>
2016-10-14 17:07:16 +0900

    fix nats clients

--- clients/nats/consumer.py
+++ clients/nats/consumer.py
@@ -3,6 +3,7 @@
 
 import timeit
 from nats.io import Client
+import tornado.gen
 import tornado.ioloop
 
 TOPIC = "t"
@@ -12,19 +13,21 @@ MSGSIZE = (1000, 100000, 1000)  # min, max, step
 COUNT = 1000
 
 def handler(body):
-    if body == "quit":
+    if body.data == "quit":
+        print body
         tornado.ioloop.IOLoop.instance().stop()
 
+@tornado.gen.coroutine
 def test():
     client = Client()
     options = {"servers": ["nats://%s:%d" % (NATS_HOST, NATS_PORT)]}
-    client.connect(**options)
-    client.subscribe(TOPIC, "workers", handler)
+    yield client.connect(**options)
+    yield client.subscribe(TOPIC, "", handler)
 
 if __name__ == "__main__":
     for size in range(*MSGSIZE):
-        result = timeit.timeit("test()",
-                               setup="from __main__ import test; import tornado.ioloop",
+        result = timeit.timeit("test();tornado.ioloop.IOLoop.instance().start()",
+                               setup="from __main__ import test;import tornado.ioloop",
                                number=1)
         print("processing time: %f" % result)
 
--- clients/nats/producer.py
+++ clients/nats/producer.py
@@ -3,6 +3,8 @@
 
 import timeit
 from nats.io import Client
+import tornado.gen
+import tornado.ioloop
 
 TOPIC = "t"
 NATS_HOST = "127.0.0.1"
@@ -10,21 +12,25 @@ NATS_PORT = 4222
 MSGSIZE = (1000, 100000, 1000)  # min, max, step
 COUNT = 1000
 
-def test(size):
+size = 0
+
+@tornado.gen.coroutine
+def test():
     client = Client()
     options = {"servers": ["nats://%s:%d" % (NATS_HOST, NATS_PORT)]}
-    client.connect(**options)
+    yield client.connect(**options)
     msg = "0" * size
     for i in range(0, COUNT):
-        client.publish(TOPIC, msg)
-    client.publish(TOPIC, "quit")
-    client.close()
+        yield client.publish(TOPIC, msg)
+    yield client.publish(TOPIC, "quit")
+    yield client.flush()
 
 if __name__ == "__main__":
-    for size in range(*MSGSIZE):
+    for s in range(*MSGSIZE):
+        size = s
         print("message size: %d" % size)
-        result = timeit.timeit("test(%d)" % size,
-                               setup="from __main__ import test",
+        result = timeit.timeit("tornado.ioloop.IOLoop.instance().run_sync(test)",
+                               setup="from __main__ import test;import tornado.ioloop",
                                number=1)
         print("processing time: %f" % result)
 
